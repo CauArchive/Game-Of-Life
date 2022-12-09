3e67fe63b444a9e518a0e78265122d96b2fe9537
HanSolo.CAU <hansnam123@gmail.com>
2022-12-09 22:39:01 +0900

    모든 셀 죽은 상태인지 확인

--- test/com/holub/life/NeighborhoodTest.java
+++ test/com/holub/life/NeighborhoodTest.java
@@ -8,7 +8,8 @@ import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NeighborhoodTest {
-    Cell cell;
+    private Cell cell;
+    private final Cell[][] grid = new Cell[8][8];
     @BeforeEach
     void setup(){
         cell = new Resident();
@@ -16,14 +17,19 @@ class NeighborhoodTest {
     // 모든 셀 죽었는지 확인하는 테스트
     @Test
     void checkAllCellDead() {
-        final Cell[][] grid = new Cell[8][8];
+
         for( int row = 0; row < 8; ++row )
             for( int column = 0; column < 8; ++column )
-                grid[row][column] = cell.create();
+                grid[row][column] = cell;
 
         for( int row = 0; row < 8; ++row )
             for( int column = 0; column < 8; ++column )
                 assertFalse(grid[row][column].isAlive());
+    }
+
+    @Test
+    void getNextState_noNeighbors(){
+        //grid[1][1]
     }
 
 }
